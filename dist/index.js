04e86e0dcfde2fc49c9117fa72a600adfd783973
David Boike <david.boike@gmail.com>
2021-11-17 16:36:59 -0600

    try again

--- dist/index.js
+++ dist/index.js
@@ -8460,33 +8460,26 @@ try {
     const testInput = core.getInput('test-input');
     core.info('Test Input: ' + testInput);
 
-    core.info(__dirname);
+    let setupPath = path.join(path.dirname(__dirname), 'setup.ps1');
+    core.info('Setup path: ' + setupPath);
 
-    let ps1Path = path.join(path.dirname(__dirname), 'setup.ps1');
-    core.info(ps1Path);
+    let commandArg = '". ' + setupPath + '"';
 
-    //console.log(JSON.stringify(github));
+    let pwsh = spawn('pwsh', ['-command', commandArg]);
 
-    // let setupPath = path.join(github.context.action_path, 'setup.ps1');
-    // console.log('Setup path: ' + setupPath);
+    ls.stdout.on('data', (data) => {
+        console.log(data);
+    });
 
-    // let commandArg = '". ' + setupPath + '"';
+    ls.stderr.on('data', (data) => {
+        console.error(data);
+    });
 
-    // let pwsh = spawn('pwsh', ['-command', commandArg]);
-
-    // ls.stdout.on('data', (data) => {
-    //     console.log(data);
-    // });
-
-    // ls.stderr.on('data', (data) => {
-    //     console.error(data);
-    // });
-
-    // ls.on('close', (code) => {
-    //     if (code !== 0) {
-    //         core.setFailed(`pwsh exited with code ${code}`);
-    //     }
-    // });
+    ls.on('close', (code) => {
+        if (code !== 0) {
+            core.setFailed(`pwsh exited with code ${code}`);
+        }
+    });
 
 } catch (error) {
     core.setFailed(error.message);
